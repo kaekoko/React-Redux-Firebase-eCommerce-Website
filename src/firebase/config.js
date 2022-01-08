3b469a9d005cbd89551f46cd9571b6c23d2e88e4
kaekoko <kekoko.mm@gmail.com>
2022-01-08 22:08:02 +0630

    hello

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,2 +1,10 @@
 // firebase config
-export const firebaseConfig = {};
+export const firebaseConfig = {
+    apiKey: "AIzaSyC6iufJA8qRmexd_AGxyBtrifcgrqohuWc",
+    authDomain: "ecommerce-react-92f58.firebaseapp.com",
+    projectId: "ecommerce-react-92f58",
+    storageBucket: "ecommerce-react-92f58.appspot.com",
+    messagingSenderId: "33304771560",
+    appId: "1:33304771560:web:cfa9ea4217794e7a0ec25d",
+    measurementId: "G-E5YTELQ02F"
+};
